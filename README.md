0a07bfbfa5d6af4cc946c02befe1085a3f4d706b
Jae Kim <jaeshinkim321@gmail.com>
2020-09-22 18:38:41 -0700

    added local storage and updated read me

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weather-dash
+
+This is a weather dashboard to show the current weather along with a five day forecast of a city that has been searched. It utilizes two weather api to retrieve the various weather information about a city worldwide. This application also utilizes local storage to save the information on the city that has already been retrieved. 
+
+Retrieving the various information through the weather api was a little difficult as all the information needed was not available through a single api. Once the city weather, longitude, and latitude was retrieved from the first api, another api was used with the retrieved information to find the UV index of the particular city along with its five day forecast. The combination of two api's were enough to retrieve adequate weather information for the application. 
+
+![Weather Dashboard](Assets/weatherdash.png)
--- javascript.js
+++ javascript.js
@@ -1,9 +1,25 @@
+// Variables
 var cityView = $('.cities-view');
-var currentTime = moment().format('l')
-var fiveDay = $('.five-day')
+var currentTime = moment().format('l');
+var fiveDay = $('.five-day');
+var cities = [];
 
-$('#search').on('submit', function (e) {
+// Get Local Storage information
+var getCity = JSON.parse(localStorage.getItem('city'));
+if (getCity !== null){
+    getCity.forEach(function(item){
+        cities.push(item)
+        })
+    for(i=0; i<cities.length; i++){
+        cityView.append(`<li class = 'list-group-item' id = city>${cities[i].charAt(0).toUpperCase()+cities[i].slice(1)}</li)`)
+    }
+}
+
+$('#search').on('submit', function(e) {
     e.preventDefault();
+// $('#search').on('submit', search())
+// console.log(city)
+// function search(){
     var city = $('#city-search').val();
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=cb24f0395a525f6bd303942171279ad4"
     
@@ -24,7 +40,12 @@ $('#search').on('submit', function (e) {
         $('.cityTemp').text(`Temperature (F): ${Math.floor((cityTemp))}`)
         $('.cityHum').text(`Humidity: ${cityHum}%`)
         $('.cityWind').text(`Wind Speed: ${cityWind} MPH`)
-        // Ajax response to get UV index
+        $('.city-weather').css("border", "1px solid black")
+        $('.five-day').css("background-color", "skyblue")
+        $('.five-day').css("color", "white")
+      
+        
+        // Ajax response to get UV index & 5 day forecast
         var uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +"&appid=cb24f0395a525f6bd303942171279ad4"
         $.ajax({
             url: uvURL,
@@ -33,6 +54,15 @@ $('#search').on('submit', function (e) {
             console.log(response)
             var uvIndex = response.current.uvi
             $('.cityUV').text(`UV Index: ${uvIndex}`)
+            // if(uvIndex  <= 2)
+            // $('.cityUV').css("color", "green")
+            //  else if (uvIndex >=3 && uvIndex <=5)
+            //  $('.cityUV').css("color", "salmon")
+            //  else if (uvIndex >= 5 && uvIndex <=7)
+            //     $('.cityUV').css("color", "blue")
+            //  else (uvIndex >= 8)
+            //     $('.cityUV').css("color", "red")
+            
             // Creates 5 day forecast
             for (i=1;i<6;i++){
             $('.fc-date' + i).text(moment().add(i, 'days').format('l'))
@@ -40,11 +70,19 @@ $('#search').on('submit', function (e) {
             $('.day-forecast-temp'+ i).text(`Temp (F): ${Math.floor(response.daily[i].temp.day -273.15)*1.80+32}`)
             $('.day-forecast-hum'+ i).text(`Humidity: ${response.daily[i].humidity}%`)
             }
-            
+        
         })
-
+       $('#search').children('input').val('');
     });
+
+    
+
+    // Set Local Storage information
+    cities.push(city)
+    localStorage.setItem('city', JSON.stringify(cities))
 
 })
 
-
+$(document).on("click", ".cities-view", function(){
+    console.log("hi")
+})
